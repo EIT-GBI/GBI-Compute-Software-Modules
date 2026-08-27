825144ae2c983093cdd8d497e3a1d5c4d66eb702
Johannes Blaschke <johannes.blaschke@gmail.com>
2026-08-27 17:22:54 +0100

    fix linux installer

--- parallel-tar/sm-config/install.sh
+++ parallel-tar/sm-config/install.sh
@@ -4,7 +4,7 @@
 GNU_SUFFIX="-gnu.2.32"
 if [[ $(to_lower "$RUNTIME_OS") == "linux" ]]
 then
-    GLIBC_VER=$(ldd --version 2>&1 | head -1 | grep -oE '[0-9]+\.[0-9]+' | tail -1)
+    GLIBC_VER=$(ldd --version 2>&1 | head -1 | grep -oE '[0-9]+\.[0-9]+' | tail -1) || true
     if [[ -n ${GLIBC_VER} && $(printf '%s\n%s\n' "2.43" "${GLIBC_VER}" | sort -V | head -1) == "2.43" ]]
     then
         GNU_SUFFIX="-gnu.2.43"
